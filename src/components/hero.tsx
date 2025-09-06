0faea9c72319531f55282887ee7b57a02b890167
besshy-MBP-2019 <170390031+tyasuda0850@users.noreply.github.com>
2025-09-06 12:38:53 +0900

    ~section 3.7

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,3 +1,5 @@
+import styles from '@/styles/hero.module.css';
+
 export default function Hero({
   title,
   subtitle,
@@ -9,8 +11,10 @@ export default function Hero({
 }) {
   return (
     <div>
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
+      <div className={styles.text}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.subtitle}>{subtitle}</p>
+      </div>
       {imageOn && <figure> [画像] </figure>}
     </div>
   );
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,5 +1,10 @@
 import Link from 'next/link';
+import styles from '@/styles/logo.module.css';
 
-export default function Logo() {
-  return <Link href="/">CUBE</Link>;
+export default function Logo({ boxOn = false }: { boxOn: boolean }) {
+  return (
+    <Link href="/">
+      <div className={boxOn ? styles.box : styles.basic}>LOGO</div>
+    </Link>
+  );
 }
